f2afd892aa348e6efced215dbf73dca6abafe7c0
Lucas Moreira <l.moreira@Live.ca>
2017-01-30 15:45:57 -0500

    Split Vue-X storage into discreet files.

--- vue-app-base/src/store/store.js
+++ vue-app-base/src/store/store.js
@@ -2,70 +2,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+// Getter Import
+import * as getters from './actions.js'
+// Mutation Imports
+import * as mutations from './mutations.js'
+// Action Imports
+import * as actions from './actions.js'
+
+// [ Module Imports ]
+import counter from './modules/counter.js'
+
 // Tell Vue to use VueX
 Vue.use(Vuex);
 
 // State Bucket..
 const state = {
-  counter: 0
+  value: 0
 };
 
-//TODO
-//
-//Separate Getters/actions/mutations into their own files
-//
-// - Getters
-// - Actions
-// - Mutations
-
-// Getters
-const getters = {
-  doubleCounter: state => {
-    return state.counter * 2;
-  },
-  stringCounter: state => {
-    return state.counter + ' Clicks';
-  }
+// [ Modules ]
+const modules = {
+  counter
 };
-
-// Mutations
-const mutations = {
-  increment: (state, payload) => {
-    state.counter += payload;
-  },
-  decrement: (state, payload) => {
-    state.counter += payload;
-  }
-
-};
-
-
-// Actions
-const actions ={
-  // Context gives access to all methods..
-  // including commit.
-  increment: (context, payload) => {
-    // Async code...
-    context.commit('increment', payload);
-  },
-  // This es6 syntax ({method}) will
-  // deconstruct the object and pull only commit
-  decrement: ({ commit }, payload) => {
-    // Async Code...
-    commit('decrement', payload);
-  },
-  asyncIncrement: ({ commit }, payload) => {
-    setTimeout (()=>{
-      commit('increment', payload);
-    }, 1000);
-  }
-};
-
 
 // Export package
 export default new Vuex.Store({
   state,
   getters,
   mutations,
-  actions
+  actions,
+  modules
 });
