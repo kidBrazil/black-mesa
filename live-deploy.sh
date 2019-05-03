246a7bba594998adb45a29cbc1bdd82bce2eccc5
Lucas Moreira <lucas@moreiradevelopment.io>
2019-05-03 14:25:09 -0400

    Updated deploy script with confirmation

--- live-deploy.sh
+++ live-deploy.sh
@@ -28,26 +28,31 @@ echo
 sleep 1s
 read -p "${YELLOW}Are you absolutely sure? please type YES or NO (case sensitive)${NC}  " confirmation
 
-# Clean old build files
-echo "${YELLOW}[ Cleaning old build files ]${NC}"
-npm run clean
-# Build from most recent master
-echo "${GREEN}[ Building from Master ]${NC}"
-git fetch && git checkout master
-npm run clean
-npm run build
-# Deploy dist folder to AWS
-echo "${GREEN}[ Deploying to AWS ${S3_LIVE_BUCKET} ]${NC}"
-if aws s3 sync dist $S3_LIVE_BUCKET
+if [ $confirmation == "YES" ]
 then
-  echo "${GREEN}[ Deploying Changes to Cloudfront ]${NC}"
-  # INVALIDATE RECORDS ON MAIN DISTRIBUTION
-  aws cloudfront create-invalidation --distribution-id $CF_MAIN_DISTRIBUTION --paths /
-  # INVALIDATE RECORDS ON SECONDARY DISTRIBUTION
-  aws cloudfront create-invalidation --distribution-id $CF_REDIRECT_DISTRIBUTION --paths /
-  echo "${GREEN}[ Changes Successfully Deployed to AWS! ]${NC}"
+  # Clean old build files
+  echo "${YELLOW}[ Cleaning old build files ]${NC}"
   npm run clean
-  exit 0
+  # Build from most recent master
+  echo "${GREEN}[ Building from Master ]${NC}"
+  git fetch && git checkout master
+  npm run clean
+  npm run build
+  # Deploy dist folder to AWS
+  echo "${GREEN}[ Deploying to AWS ${S3_LIVE_BUCKET} ]${NC}"
+  if aws s3 sync dist $S3_LIVE_BUCKET
+  then
+    echo "${GREEN}[ Deploying Changes to Cloudfront ]${NC}"
+    # INVALIDATE RECORDS ON MAIN DISTRIBUTION
+    aws cloudfront create-invalidation --distribution-id $CF_MAIN_DISTRIBUTION --paths /
+    # INVALIDATE RECORDS ON SECONDARY DISTRIBUTION
+    aws cloudfront create-invalidation --distribution-id $CF_REDIRECT_DISTRIBUTION --paths /
+    echo "${GREEN}[ Changes Successfully Deployed to AWS! ]${NC}"
+    npm run clean
+    exit 0
+  else
+    error_handle "Something went wrong while deploying to AWS"
+  fi
 else
-  error_handle "Something went wrong while deploying to AWS"
+  error_handle "Try again when ready"
 fi
